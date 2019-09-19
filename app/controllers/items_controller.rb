fb505c34fb66392d9507afd511af65652b9d5e0d
Dan <dancorpx@gmail.com>
2019-09-19 16:41:24 +0800

    fixed bugs

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,6 +31,19 @@ class ItemsController < ApplicationController
       end
     end
 
+    def update
+      respond_to do |format|
+        if @item.update(item_params)
+          format.html { redirect_to items_path, notice: 'item was successfully updated.' }
+          format.json { render :show, status: :ok, location: @item }
+        else
+          format.html { render :edit }
+          format.json { render json: @item.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+
     def destroy
       @item.destroy
       respond_to do |format|
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_041845) do
+ActiveRecord::Schema.define(version: 2019_09_18_083349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_09_18_041845) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "roasts", force: :cascade do |t|
@@ -59,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_09_18_041845) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
